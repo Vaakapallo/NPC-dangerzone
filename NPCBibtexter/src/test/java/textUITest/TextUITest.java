8bbd71d122349b5e396cfd85e3188ae10b4d0241
Tomi Virtanen <cobrelli@gmail.com>
2013-03-25 14:09:54 +0200

    testiluokka

---
+++ NPCBibtexter/src/test/java/textUITest/TextUITest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textUITest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author vito
+ */
+public class TextUITest extends TestCase {
+    
+    public TextUITest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+    
+    public void testtrololo(){
+        
+    }
+}
