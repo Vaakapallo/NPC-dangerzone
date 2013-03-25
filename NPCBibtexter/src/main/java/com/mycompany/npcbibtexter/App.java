d21c41f4454086804c1d0866bea511bf237d5b90
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-03-25 09:35:45 +0200

    Maven-projekti luotu, ny kooraillaan

---
+++ NPCBibtexter/src/main/java/com/mycompany/npcbibtexter/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.npcbibtexter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
