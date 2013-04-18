81021f1e2660fa51817460555dac59f424a5b409
Lauri <Lauri@192.168.2.5>
2013-04-19 00:24:28 +0300

    Kirjassa muutettu IsValid() ottamaan huomioon Author/Editor, tulostuksessakin tulostetaan vain toinen, Authorin paikalle requiredFieldeissa on laitettu Field, jonka alaluokkia metodeissa tarkastellaan.

--- NPCBibtexter/src/main/java/Entries/Book.java
+++ NPCBibtexter/src/main/java/Entries/Book.java
@@ -16,7 +16,7 @@ import java.util.HashSet;
  */
 public class Book extends Entry {
 //    final static private Class[] requiredFields = {Author.class, Editor.class, Title.class, Publisher.class, Year.class};
-    final static private Class[] requiredFields = {Author.class, Title.class, Publisher.class, Year.class};
+    final static private Class[] requiredFields = {Field.class, Title.class, Publisher.class, Year.class};
     final static private Class[] optionalFields = {};
     final public HashSet optionalSet = new HashSet(Arrays.asList(optionalFields));
     String tag;
@@ -36,10 +36,15 @@ public class Book extends Entry {
     @Override
     public boolean isValid() {
         HashMap<Class<? extends Field>, Field> tmpHashMap = (HashMap) list.clone();
-        if (!(tmpHashMap.containsKey(Author.class)) && !(tmpHashMap.containsKey(Editor.class))) {
+        if (!(tmpHashMap.containsKey(Author.class.asSubclass(Field.class))) && !(tmpHashMap.containsKey(Editor.class.asSubclass(Field.class))) || ((tmpHashMap.containsKey(Author.class.asSubclass(Field.class))) && (tmpHashMap.containsKey(Editor.class.asSubclass(Field.class))))) {
             return false;
         }
-        for (int i = 2; i < requiredFields.length; i++) {
+        if(tmpHashMap.containsKey(Author.class.asSubclass(Field.class))){
+            tmpHashMap.remove(Author.class.asSubclass(Field.class));
+        } else {
+            tmpHashMap.remove((Editor.class.asSubclass(Field.class)));
+        }
+        for (int i = 1; i < requiredFields.length; i++) {
             Class field = requiredFields[i];
             if (!tmpHashMap.containsKey(field)) {
                 return false;
@@ -59,10 +64,16 @@ public class Book extends Entry {
 
     @Override
     public String toString() {
+        
         String resString = "@Book{ " + tag + ",\n";
-        for (Class class1 : requiredFields) {
-            if (list.containsKey(class1)) {
-                resString = resString + list.get(class1);
+        if(list.containsKey(Author.class.asSubclass(Field.class))){
+            resString = resString + list.get(Author.class.asSubclass(Field.class));
+        } else {
+           resString = resString + list.get(Editor.class.asSubclass(Field.class));
+        }
+        for (int i = 1; i < requiredFields.length; i++) {
+            if (list.containsKey(requiredFields[i])) {
+                resString = resString + list.get(requiredFields[i]);
             }
         }
         for (Class class1 : optionalFields) {
