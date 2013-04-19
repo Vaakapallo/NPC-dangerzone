b7193c41d5e1fe48c33178e34999e87e09e24b9b
cobrelli <cobrelli@gmail.com>
2013-04-19 10:08:25 +0300

    testien ja editin refaktorointia vastaamaan muuttunutta entrystoragea

--- NPCBibtexter/src/test/java/CommandInterpreterTest/EditReferenceTest.java
+++ NPCBibtexter/src/test/java/CommandInterpreterTest/EditReferenceTest.java
@@ -44,21 +44,21 @@ public class EditReferenceTest extends TestCase {
         EntryStorage.empty();
     }
 
-//    public void testEditingAddsNewReferenceWhenRightKey() {
-//        String[] input = {"jj1", "Nawb, Jerry", "Starcraft ownage", "Starcraft II Ownage 101", 
-//            "2013", "jj1", "jj1", "Nawb, Jerry", "Starcraft pwnage",  "Starre II", "1337"};
-//        IOStub io = new IOStub(input);
-//        new AddReference(io).run();
-//        new EditReference(io).run();
-//        
-//        String output = "";
-//        for (String string : io.getOutput()) {
-//            output += string;
-//        }
-//
-//        assertTrue(output.contains("Starre II"));
-//        EntryStorage.empty();
-//    }
+    public void testEditingAddsNewReferenceWhenRightKey() {
+        String[] input = {"jj1", "Nawb, Jerry", "Starcraft ownage", "Starcraft II Ownage 101", 
+            "2013", "jj1", "jj1", "Nawb, Jerry", "Starcraft pwnage",  "Starre II", "1337"};
+        IOStub io = new IOStub(input);
+        new AddReference(io).run();
+        new EditReference(io).run();
+        
+        String output = "";
+        for (String string : io.getOutput()) {
+            output += string;
+        }
+
+        assertTrue(output.contains("Starre II"));
+        EntryStorage.empty();
+    }
     
     public void testEditingDoesntAddNewReferenceWhenWrongKey() {
         String[] input = {"jj1", "Nawb, Jerry", "Starcraft ownage", "Starcraft II Ownage 101", 
