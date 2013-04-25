25902154d354c3597fabaa6850fd0806f0efe42b
cobrelli <cobrelli@gmail.com>
2013-04-25 09:35:46 +0300

    testejä

--- NPCBibtexter/src/test/java/ambassadorTest/BibmakeupartistTest.java
+++ NPCBibtexter/src/test/java/ambassadorTest/BibmakeupartistTest.java
@@ -5,7 +5,8 @@
 package ambassadorTest;
 
 import Entries.Entry;
-import Entries.Inproceedings;
+import Fields.Edition;
+import Fields.Series;
 import ambassador.Bibmakeupartist;
 import applicationLogic.Build;
 import java.io.ByteArrayOutputStream;
@@ -42,8 +43,12 @@ public class BibmakeupartistTest extends TestCase {
         Bibmakeupartist mu = new Bibmakeupartist();
         List<Entry> entries = new ArrayList<Entry>();
         entries.add(Build.Inproceedings("lol", "karhu", "uli", 1556, "sinep"));
+        entries.get(0).list.put(Edition.class, new Edition("edition"));
+        entries.get(0).list.put(Series.class, new Series("serie"));
         mu.printSexyStringsToUser(entries);
         assertTrue(outContent.toString().contains("karhu"));
+        assertTrue(outContent.toString().contains("serie"));
+        assertTrue(outContent.toString().contains("edition"));
     }
 
     public void testArticlePrintsWhatAsked() {
@@ -61,6 +66,8 @@ public class BibmakeupartistTest extends TestCase {
         mu.printSexyStringsToUser(entries);
         assertTrue(outContent.toString().contains("uli"));
     }
+    
+    
     // TODO add test methods here. The name must begin with 'test'. For example:
     // public void testHello() {}
 }
